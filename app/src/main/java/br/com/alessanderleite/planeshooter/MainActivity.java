de67de28b62b5e15684a98273cf2498b74feda22
alessanderleite <alessander.logistica@gmail.com>
2019-02-20 16:39:46 -0300

    change extention for MainActivity

--- app/src/main/java/br/com/alessanderleite/planeshooter/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/planeshooter/MainActivity.java
@@ -1,13 +1,18 @@
 package br.com.alessanderleite.planeshooter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View v) {
+
     }
 }
