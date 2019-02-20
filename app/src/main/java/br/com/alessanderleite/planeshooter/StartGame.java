91e68ad0fc7d4fa2852f37697c14a010b2d6df26
alessanderleite <alessander.logistica@gmail.com>
2019-02-20 16:44:12 -0300

    create a new java class named StartGame

---
+++ app/src/main/java/br/com/alessanderleite/planeshooter/StartGame.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite.planeshooter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class StartGame extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
