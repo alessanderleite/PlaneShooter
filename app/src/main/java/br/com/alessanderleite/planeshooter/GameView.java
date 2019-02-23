3c44cdfe2cf7cff4bc5881be3548c2f36bb4d4ee
alessanderleite <alessander.logistica@gmail.com>
2019-02-22 21:58:02 -0300

    Declare the objects Rect, Display and Point inside GameView to ajust the background image in the screen

--- app/src/main/java/br/com/alessanderleite/planeshooter/GameView.java
+++ app/src/main/java/br/com/alessanderleite/planeshooter/GameView.java
@@ -1,24 +1,37 @@
 package br.com.alessanderleite.planeshooter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.view.Display;
 import android.view.View;
 
 public class GameView extends View {
 
     Bitmap background;
+    Rect rect;
+    int dWidth, dHeight;
 
     public GameView(Context context) {
         super(context);
         background = BitmapFactory.decodeResource(getResources(), R.drawable.background);
+        Display display = ((Activity)getContext()).getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        dWidth = size.x;
+        dHeight = size.y;
+        rect = new Rect(0,0,dWidth,dHeight);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawBitmap(background,0,0,null);
+        canvas.drawBitmap(background,null,rect,null);
 
     }
 }
