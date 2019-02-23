5aac38a5854c97b0025479d45b1960510415c4f2
alessanderleite <alessander.logistica@gmail.com>
2019-02-23 18:42:31 -0300

    create the empty class named Plane2

---
+++ app/src/main/java/br/com/alessanderleite/planeshooter/Plane2.java
@@ -0,0 +1,9 @@
+package br.com.alessanderleite.planeshooter;
+
+import android.content.Context;
+
+public class Plane2 extends Plane {
+    public Plane2(Context context) {
+        super(context);
+    }
+}
