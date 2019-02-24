def12e85b148d472202677e211a30bd3aaf21ce9
alessanderleite <alessander.logistica@gmail.com>
2019-02-23 21:36:51 -0300

    set flag_fullscreen

--- app/src/main/java/br/com/alessanderleite/planeshooter/StartGame.java
+++ app/src/main/java/br/com/alessanderleite/planeshooter/StartGame.java
@@ -3,6 +3,8 @@ package br.com.alessanderleite.planeshooter;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class StartGame extends Activity {
 
@@ -11,6 +13,9 @@ public class StartGame extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         gameView = new GameView(this);
         setContentView(gameView);
     }
